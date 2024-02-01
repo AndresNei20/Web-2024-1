28d92e5cc4608a7a80dc847282449b0f3bcee622
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-02-01 18:52:53 -0500

    nested app component with exports

---
+++ first-vanilla-class/src/components/Body/Body.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Body = () => {
+  return (
+    <main>hello i'm 2nd</main>
+  )
+}
---
+++ first-vanilla-class/src/components/Header/Header.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Header = () => {
+  return (
+    <header>hello i'm 1st</header>
+  )
+}
