15db6ac9c8d0ffcf5203529843632a029f636887
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-02-03 23:25:29 -0500

    some cards and css

--- first-vanilla-class/src/components/Body/Body.jsx
+++ first-vanilla-class/src/components/Body/Body.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
+import { MoviesCards } from './MoviesCards/MoviesCards'
+import { LargeMovieCards } from './LargeMovieCards/LargeMovieCards'
 
 export const Body = () => {
   return (
-    <main>hello i'm 2nd</main>
+    <main>
+      <h4>Recommended movies</h4>
+      <MoviesCards></MoviesCards>
+      <h4>Amazon originals</h4>
+      <LargeMovieCards></LargeMovieCards>
+      <h4 className='yellow_titles'>Popular movies</h4>
+    </main>
   )
 }
---
+++ first-vanilla-class/src/components/Body/LargeMovieCards/LargeMovieCards.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const images = [
+    {src:'https://lh3.googleusercontent.com/proxy/Y5fK6G6irOjUsTVnZH4thl0nwfkDeH5qgBQHLXydxOyWck18J2dmSFAlSckG-qM7IoUG-_t5Iso2U9b0iTEMk11PovYhgby44L8BVoRukb3ynnVmYwibmKRF', title: 'rigo'},
+    {src:'https://pbs.twimg.com/media/F_sczsLWkAAft0C?format=jpg&name=4096x4096', title: 'zorro'},
+    {src:'https://preview.redd.it/new-poster-for-saltburn-v0-hin6xt72qrwb1.jpg?width=640&crop=smart&auto=webp&s=004ad6d52da353b32835bc9c288f1faacd6f4fee', title: 'saltburn'},
+    {src:'https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fseries%2F368207%2Fposters%2F652a5ef981eab.jpg&w=640&q=75', title: 'invincible'},
+    {src:'https://lh3.googleusercontent.com/proxy/Y5fK6G6irOjUsTVnZH4thl0nwfkDeH5qgBQHLXydxOyWck18J2dmSFAlSckG-qM7IoUG-_t5Iso2U9b0iTEMk11PovYhgby44L8BVoRukb3ynnVmYwibmKRF', title: 'rigo'},
+    {src:'https://pbs.twimg.com/media/F_sczsLWkAAft0C?format=jpg&name=4096x4096', title: 'zorro'},
+    {src:'https://preview.redd.it/new-poster-for-saltburn-v0-hin6xt72qrwb1.jpg?width=640&crop=smart&auto=webp&s=004ad6d52da353b32835bc9c288f1faacd6f4fee', title: 'saltburn'},
+    {src:'https://www.tvtime.com/_next/image?url=https%3A%2F%2Fartworks.thetvdb.com%2Fbanners%2Fv4%2Fseries%2F368207%2Fposters%2F652a5ef981eab.jpg&w=640&q=75', title: 'invincible'},
+    
+]
+
+export const LargeMovieCards = () => {
+  return (
+    <div className='large_movie_cards'>
+        {images.map((image, index) => (
+            <img className='large_cards' key={index} src={image.src} alt={image.title} />
+        ))}
+    </div>
+  )
+}
