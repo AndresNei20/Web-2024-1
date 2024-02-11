d88143395352bfdedfdc9fb03dd5a04e0eefa074
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-02-11 17:30:09 -0500

    header with nav and  half of css

--- first-vanilla-class/src/components/Header/Header.jsx
+++ first-vanilla-class/src/components/Header/Header.jsx
@@ -1,10 +1,54 @@
-import React from 'react'
-import { Nav } from './Nav/Nav'
+import React from "react";
+import icon1 from '../../assets/icons/search.svg'
+import { menu_items } from "../../data/menu_items";
+
+
+console.log(menu_items  )
 
 export const Header = () => {
   return (
-    <header>
-      <Nav></Nav>
+    <header >
+      <div className="navbar">
+        <button className="menu_btn">
+          Menu
+        </button>
+
+        <nav>
+          <ul className="menu_list">
+            {
+              menu_items.map(({id, text, icon}) => {
+                return(
+                  <li key={id}>
+                    <img src={icon} alt={text}/> {text}
+                  </li>
+                )
+              })
+            }
+          </ul>
+        </nav>
+
+        <a className="title" href="">
+          <strong>prime video</strong>
+        </a>
+
+        <a href="">
+          <img
+            width="30px"
+            height="30px"
+            src= {icon1}
+            alt="search--v1"
+          />
+        </a>
+
+        <a href="">
+          <img
+            width="30px"
+            height="30px"
+            src="https://m.media-amazon.com/images/G/02/CerberusPrimeVideo-FN38FSBD/adult-1.png"
+            alt="search--v1"
+          />
+        </a>
+      </div>
     </header>
-  )
-}
+  );
+};
