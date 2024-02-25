79095c50481a73ae09e99b421ad5b8659cdfe767
AndresNei20 <andresfelipeneira2004@gmail.com>
2024-02-25 18:39:08 -0500

    helper folder deleted

--- counter-app/src/helpers/fetchData.js
+++
@@ -1,8 +0,0 @@
-export const fetchData = async(url) => {
-    try {
-        const response = fetch(url);
-        return await response.json()
-    } catch (error) {
-        throw new error ('Error fetching Data')
-    }
-}
