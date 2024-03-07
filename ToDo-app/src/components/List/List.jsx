3ae567026b0cc9b4c31f60ec10049ac623e518a8
Paola Andrea Velasquez <paovelasquezd2002@gmail.com>
2024-03-06 19:12:38 -0500

    added completed:true

--- ToDo-app/src/components/List/List.jsx
+++ ToDo-app/src/components/List/List.jsx
@@ -1,25 +1,55 @@
 import PropTypes from 'prop-types'
+import React, { useState, useEffect } from 'react';
 import './List.css'
 
-export const List = ({todos}) => {
-    console.log(todos)
+export const List = ({ todos }) => {
+    const [tasks, setTasks] = useState([]);
+
+    useEffect(() => {
+        const initializedTasks = todos.map((todo) => ({
+          text: todo.text,
+          completed: todo.completed || false,
+        }));
+        setTasks(initializedTasks);
+      }, [todos]);
+    
+    
+    const handleCheckboxChange = (index) => {
+        const updatedTasks = [...tasks];
+        updatedTasks[index].completed = !updatedTasks[index].completed;
+        todos[index].completed = updatedTasks[index].completed;
+        setTasks(updatedTasks);
+      };
+    
+    const handleDeleteButtonClick = (index) => {
+    const updatedTasks = tasks.filter((_, i) => i !== index);
+    setTasks(updatedTasks);
+  };
+    
+  console.log(todos)
+
     return (
         <div>
             <h2>Your Tasks</h2>
             <ul>
                 {
-                    todos.map((todo, index)=>(
+                    todos.map((todo, index) => (
                         <div className='task-container'
-                             key={index}>
-                        <li>
-                            <div className='task-label'>
-                                <input type="checkbox" />
-                            {todo.text}
-                            </div>
-                        </li>
-                        <button>
-                            Delete
-                        </button>
+                            key={index}>
+                            <li>
+                                <div className='task-label'>
+                                    <input
+                                        type="checkbox"
+                                        checked={todo.completed || false}
+                                        onChange={() => handleCheckboxChange(index)}
+                                    />
+                                    
+                                    {todo.text}
+                                </div>
+                            </li>
+                            <button onClick={() => handleDeleteButtonClick(index)}>
+                                Delete
+                            </button>
                         </div>
                     ))
                 }
